2ece48aee036577cda4a3e0149256ad2dfd35e72
vishalvarghese46 <vishalvarghese46@gmail.com>
2022-02-13 18:07:36 +0000

    working with imports

--- hello_world.js
+++ hello_world.js
@@ -1,1 +1,4 @@
-console.log("Hello World")
+const {get_last_name, get_full_name} = require("./app");
+
+console.log(`Hello ${get_full_name("Vishal", "Varghese")}`);
+console.log(`Again ${get_last_name("Varghese")}`);
